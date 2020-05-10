9638ddd34c11fd39d72c5b08b852d31095f0f50f
Gustavo Silveira <gusilveiramp@gmail.com>
2020-05-10 12:22:49 -0300

    migrations

--- app/Models/User.js
+++ app/Models/User.js
@@ -7,7 +7,7 @@ const Model = use('Model')
 const Hash = use('Hash')
 
 class User extends Model {
-  static boot () {
+  static boot() {
     super.boot()
 
     /**
@@ -22,6 +22,16 @@ class User extends Model {
   }
 
   /**
+   * Traits ACL
+   */
+  static get traits() {
+    return [
+      '@provider:Adonis/Acl/HasRole',
+      '@provider:Adonis/Acl/HasPermission',
+    ]
+  }
+
+  /**
    * A relationship on tokens is required for auth to
    * work. Since features like `refreshTokens` or
    * `rememberToken` will be saved inside the
@@ -31,7 +41,7 @@ class User extends Model {
    *
    * @return {Object}
    */
-  tokens () {
+  tokens() {
     return this.hasMany('App/Models/Token')
   }
 }
---
+++ database/migrations/1589123134574_image_product_schema.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ImageProductSchema extends Schema {
+  up() {
+    this.create('image_product', (table) => {
+      table.increments()
+      table.integer('image_id').unsigned()
+      table.integer('product_id').unsigned()
+
+      table
+        .foreign('image_id')
+        .references('id')
+        .inTable('images')
+        .onDelete('cascade')
+      table
+        .foreign('product_id')
+        .references('id')
+        .inTable('products')
+        .onDelete('cascade')
+    })
+  }
+
+  down() {
+    this.drop('image_product')
+  }
+}
+
+module.exports = ImageProductSchema
